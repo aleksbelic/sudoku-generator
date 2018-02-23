00f3deed2f393de1c175965d244dcb4fad528ba3
aleksbelic <aleks.belic@gmail.com>
2018-02-23 18:39:47 +0100

    Adding execution time check.

--- sudoku-generator.py
+++ sudoku-generator.py
@@ -1,6 +1,13 @@
+import time
 import random
 
-#print("SUDOKU GENERATOR")
-tableSize = 9
-randNum = random.randrange(1, tableSize, 1)
-print(randNum)
+start_time = time.time()
+
+print("SUDOKU GENERATOR")
+table_size = 9
+table = []
+for i in range(table_size):
+    table.append([])
+randNum = random.randrange(1, table_size, 1)
+print(table)
+print("--- %s seconds ---" % (time.time() - start_time))
