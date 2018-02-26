945d2e4bf7394ce15d8457868f7c8f3d9b81ef52
Aleksandar Belic <aleks.belic@gmail.com>
2018-02-26 22:48:18 +0100

    Adding generate and check layout - not complete.

--- demo.py
+++ demo.py
@@ -3,5 +3,8 @@ from sudoku import SudokuTable, Helper
 
 #start_time = time.time()
 table = SudokuTable(9)
-#table.print_layout()
+table.print_layout()
+print(table.check_layout())
 #print("--- %s seconds ---" % (time.time() - start_time))
+
+#Helper.test()
--- sudoku.py
+++ sudoku.py
@@ -1,19 +1,48 @@
-import random
+import random, math
 
 class SudokuTable:
     """Some docstring."""
-    
     def __init__(self, size):
         self.size = size
         self.layout = []
-        for _ in range(self.size): # underscore is for unused variable
+        for _ in range(self.size):
             self.layout.append([])
+        self.generate_layout()
+
+    #TODO
+    def generate_layout(self):
+        """Generates random sudoku table layout."""
+        for row in range(self.size):
+            self.layout[row] = Helper.get_rand_unique_list(1, self.size)
+
+    #TODO
+    def check_layout(self):
+        """Checks sudoku table layout."""
+
+        # checking rows
+        for row in range(self.size):
+            if len(set(self.layout[row])) != self.size:
+                return False
+
+        #checking columns
+        for i in range(self.size):
+            column = []
+            for j in range(self.size):
+                column.append(self.layout[j][i])
+            if len(set(column)) != self.size:
+                print(set(column))
+                return False
+
+        #checking regions
+        region_count = int(math.sqrt(self.size))
+
+        return True
 
     def print_layout(self):
-        """Printing the sudoku table."""
+        """Prints sudoku table layout."""
         for row in self.layout:
             print(row)
-
+  
 class Helper:
     """Some docstring"""
 
@@ -26,3 +55,9 @@ class Helper:
     def get_rand_unique_list(min, max):
         """Returns random list of unique integers between min & max (both values included)."""
         return random.sample(range(min,max + 1), max - min + 1)
+
+    @staticmethod
+    def test():
+        """Just for testing"""
+        col = [9,8,7,7,6]
+        print(set(col))
