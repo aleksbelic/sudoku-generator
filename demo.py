c99da11c133ef39e9c04f3be9727bfa642c407e6
Aleksandar Belic <aleks.belic@gmail.com>
2018-03-22 23:32:51 +0100

    Another solving test added.

--- demo.py
+++ demo.py
@@ -5,7 +5,8 @@ from sudoku import SudokuGenerator, SudokuSolver
 #sudoku_example.print_grid()
 #Helper.store(sudoku_example) # storing grid to file ./storage/9x9.csv
 
-sudoku_puzzle = [0,0,0,2,6,0,7,0,1,6,8,0,0,7,0,0,9,0,1,9,0,0,0,4,5,0,0,8,2,0,1,0,0,0,4,0,0,0,4,6,0,2,9,0,0,0,5,0,0,0,3,0,2,8,0,0,9,3,0,0,0,7,4,0,4,0,0,5,0,0,3,6,7,0,3,0,1,8,0,0,0,]
-sudoku_solver = SudokuSolver(sudoku_puzzle)
+#sudoku_puzzle = [0,0,0,2,6,0,7,0,1,6,8,0,0,7,0,0,9,0,1,9,0,0,0,4,5,0,0,8,2,0,1,0,0,0,4,0,0,0,4,6,0,2,9,0,0,0,5,0,0,0,3,0,2,8,0,0,9,3,0,0,0,7,4,0,4,0,0,5,0,0,3,6,7,0,3,0,1,8,0,0,0,]
+sudoku_puzzle = [0,0,5,0,1,0,0,0,0,0,2,0,4,0,0,0,0,1,0,9,0,5,0,0,0,8,0,5,0,8,0,0,0,3,0,0,0,7,2,0,4,0,1,6,0,0,0,6,0,0,0,7,0,2,0,5,0,0,0,1,0,2,0,7,0,0,0,0,8,0,3,0,0,0,0,0,3,0,6,0,0,]
+sudoku_solver = SudokuSolver(sudoku_puzzle) # sudoku_puzzle, timer = False, sound = True
 sudoku_solver.solve()
 sudoku_solver.print_grid()
